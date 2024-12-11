57e343592956ab04695fb8c2b0fd88563bf3f2fe
David Isikwe <disikwe@gmail.com>
2024-12-11 16:54:29 -0600

    authentication works

--- blogr-nextjs-prisma/pages/api/auth/[...nextauth].ts
+++ blogr-nextjs-prisma/pages/api/auth/[...nextauth].ts
@@ -1,15 +1,35 @@
+// import NextAuth from 'next-auth';
+// import GithubProvider from 'next-auth/providers/github';
+// import { PrismaAdapter } from '@next-auth/prisma-adapter';
+// import prisma from '../../../lib/prisma';
+
+// export default NextAuth({
+//   adapter: PrismaAdapter(prisma),
+//   providers: [
+//     GithubProvider({
+//       clientId: process.env.GITHUB_ID!,
+//       clientSecret: process.env.GITHUB_SECRET!,
+//     }),
+//   ],
+//   secret: process.env.NEXTAUTH_SECRET,
+// });
+
+import { NextApiHandler } from 'next';
 import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import GitHubProvider from 'next-auth/providers/github';
 import prisma from '../../../lib/prisma';
 
-export default NextAuth({
-  adapter: PrismaAdapter(prisma),
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
+const options = {
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
-});
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET,
+};
